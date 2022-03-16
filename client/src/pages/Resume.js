beec85d9df5592f064aa5ee713847577266e4583
JasonDesktop <MaJason93@gmail.com>
2022-03-15 18:56:08 -0700

    Lets test the resume download button on HEROKU"

---
+++ client/src/pages/Resume.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+
+export default function Resume() {
+
+return (
+    <Box>
+<Typography align="center" variant="h2"> RESUME </Typography>
+<Button size="small" sx={{ mx: "auto" }} ><a href="/download/Jason_Ma_Resume.PDF" download="Jason_Ma_Resume.PDF" margin="auto" >CLICK TO DOWNLOAD RESUME</a></Button>
+</Box>
+);
+}
