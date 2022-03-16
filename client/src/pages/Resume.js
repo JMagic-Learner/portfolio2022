e040b7ccdb7766570203052a8549867234438794
JasonDesktop <MaJason93@gmail.com>
2022-03-15 19:19:00 -0700

    This is a test to see  if PDF vs pdf (capitlization) matters

--- client/src/pages/Resume.js
+++ client/src/pages/Resume.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import pdf from "./assets/download/Jason_Ma_Resume.pdf"
 
 
 export default function Resume() {
@@ -9,7 +10,7 @@ export default function Resume() {
 return (
     <Box>
 <Typography align="center" variant="h2"> RESUME </Typography>
-<Button size="small" sx={{ mx: "auto" }} ><a href="./assets/download/Jason_Ma_Resume.PDF" download="Jason_Ma_Resume.PDF" margin="auto" >CLICK TO DOWNLOAD RESUME</a></Button>
+<Button size="small" sx={{ mx: "auto" }} ><a href="./assets/download/Jason_Ma_Resume.pdf" download="Jason_Ma_Resume.PDF" margin="auto" >CLICK TO DOWNLOAD RESUME</a></Button>
 </Box>
 );
 }
