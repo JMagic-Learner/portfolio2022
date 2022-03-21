0fe5beb9ccb767bf5e197a68986562785b96ea78
JasonDesktop <MaJason93@gmail.com>
2022-03-20 20:26:23 -0700

    This is a test

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-   'mongodb://localhost/2022Portfolio' || process.env.MONGODB_URI,
+  process.env.MONGODB_URI || 'mongodb://localhost/2022Portfolio',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
