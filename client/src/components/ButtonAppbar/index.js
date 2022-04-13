cca3241170d3df319a24a1d2902d2ea7066686a2
JasonDesktop <MaJason93@gmail.com>
2022-04-12 19:21:55 -0700

    We are going to add more comments later

--- client/src/components/ButtonAppbar/index.js
+++ client/src/components/ButtonAppbar/index.js
@@ -9,52 +9,47 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Link from '@mui/material/Link';
 import { Grid } from '@mui/material';
 
-
+// This is the component that displays the Navigation bar
+// This component is loaded into App.js, and will be found in every instance of REACT rendering.
 export default function ButtonAppBar() {
 
   return (
-    // <Box sx={{ flexGrow: 1 }}>
-    
-      <AppBar position="relative">
-      
-        <Toolbar elevation={0}>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-             PORTFOLIO
-          </Typography>
-          <Grid container spacing={1} align="center" xs={12} sm={12}  md={12}>
-          {/* sssssssssssssssssss */}
+    <AppBar position="relative">
+
+      <Toolbar elevation={0}>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+          PORTFOLIO
+        </Typography>
+        <Grid container spacing={1} align="center" xs={12} sm={12} md={12}>
           {/* flexgrow specifically applies to the following buttons */}
-          
-          <Grid item xs={6} sm={2.75}  md={2}> 
-          <Button color="inherit" variant="h6" component={Link} sx={{ flexGrow: 1 }} href="/">Home</Button>
+
+          <Grid item xs={6} sm={2.75} md={2}>
+            <Button color="inherit" variant="h6" component={Link} sx={{ flexGrow: 1 }} href="/">Home</Button>
           </Grid>
           <Grid item xs={6} sm={2.75} md={2}>
-          <Button color="inherit" variant="h6" component={Link} sx={{ flexGrow: 1 }} href="/Bio"> Bio</Button>
+            <Button color="inherit" variant="h6" component={Link} sx={{ flexGrow: 1 }} href="/Bio"> Bio</Button>
           </Grid>
           <Grid item xs={6} sm={2.75} md={2}>
-          <Button color="inherit" variant="h6" component={Link} sx={{ flexGrow: 1 }} href="/Resume"> Resume</Button>
+            <Button color="inherit" variant="h6" component={Link} sx={{ flexGrow: 1 }} href="/Resume"> Resume</Button>
           </Grid>
           <Grid item xs={6} sm={2.75} md={2}>
-          <Button color="inherit" variant="h6" component={Link} sx={{ flexGrow: 1 }} href="/Projects"> Projects</Button>
+            <Button color="inherit" variant="h6" component={Link} sx={{ flexGrow: 1 }} href="/Projects"> Projects</Button>
           </Grid>
           <Grid item xs={6} sm={2.75} md={2}>
-          <Button color="inherit" variant="h6" component={Link} sx={{ flexGrow: 1 }} href="/Contact">Contact</Button>
+            <Button color="inherit" variant="h6" component={Link} sx={{ flexGrow: 1 }} href="/Contact">Contact</Button>
           </Grid>
-          </Grid>
-        </Toolbar>
-       
-      </AppBar>
-     
-     
-    // </Box>
+        </Grid>
+      </Toolbar>
+
+    </AppBar>
   );
 }
--- client/src/components/CertificationTable/index.js
+++ client/src/components/CertificationTable/index.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import { Grid } from '@mui/material';
 
 //Table Components
@@ -13,54 +11,54 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-
-
 // createData defines columns.
 function createData(Institution, Program, Certification, Year) {
-    return { Institution, Program, Certification, Year };
-  }
+  return { Institution, Program, Certification, Year };
+}
 
 // rows are self explanatory
-  const rows = [
-    createData('Univeristy of Washington', "Economics", "B.A", 2016),
-    createData('University of Washington', "UW Coding Bootcamp", 'Full-Stack Development', 2021),
-    createData('Department of Licensing', "Real Estate License", "Broker License", 2021),
-  ];
+const rows = [
+  createData('Univeristy of Washington', "Economics", "B.A", 2016),
+  createData('University of Washington', "UW Coding Bootcamp", 'Full-Stack Development', 2021),
+  createData('Department of Licensing', "Real Estate License", "Broker License", 2021),
+];
+
+// This shows our table of Certifications
 export default function CertificationTable() {
-    return(
-        <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}> 
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
         <Grid item xs={8} sm={8} md={8} lg={8} sx={{ m: 'auto' }}>
-        <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Institution</TableCell>
-                    <TableCell align="right">Program</TableCell>
-                    <TableCell align="right">Certification</TableCell>
-                    <TableCell align="right">Year</TableCell>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Institution</TableCell>
+                  <TableCell align="right">Program</TableCell>
+                  <TableCell align="right">Certification</TableCell>
+                  <TableCell align="right">Year</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow
+                    key={row.Institution}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">{row.Institution}</TableCell>
+                    <TableCell align="right">{row.Program}</TableCell>
+                    <TableCell align="right">{row.Certification}</TableCell>
+                    <TableCell align="right">{row.Year}</TableCell>
                   </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rows.map((row) => (
-                    <TableRow
-                      key={row.Institution}
-                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                      <TableCell component="th" scope="row">{row.Institution}</TableCell>
-                      <TableCell align="right">{row.Program}</TableCell>
-                      <TableCell align="right">{row.Certification}</TableCell>
-                      <TableCell align="right">{row.Year}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-            </Grid>
-        
-          
-        
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Grid>
-        </Box>
-    );
+
+
+
+      </Grid>
+    </Box>
+  );
 }
